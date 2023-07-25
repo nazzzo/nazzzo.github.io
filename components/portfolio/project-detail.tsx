bb108c33d8a5b60a0cb29871941243cbbf166a86
kimjh <hecticika@gmail.com>
2023-07-25 15:18:15 +0900

    fix project

--- components/portfolio/project-detail.tsx
+++ components/portfolio/project-detail.tsx
@@ -3,7 +3,7 @@ import rehypeRaw from 'rehype-raw';
 
 export const ProjectDetail = ({ details }: { details: string }) => {
   return (
-    <div className="relative w-[400px] lg:w-[600px] max-w-md px-3 py-6">
+    <div className="relative w-[372px] md-[460px] lg:w-[600px] max-w-md px-3 py-6">
       <div className="prose max-h-[70%] overflow-scroll">
         <ReactMarkdown className="dark:text-white" rehypePlugins={[rehypeRaw]}>{details}</ReactMarkdown>
       </div>
