bc68077c95eeafebebfce372a02ea83d6c360686
kimjh <hecticika@gmail.com>
2023-07-25 11:29:21 +0900

    fix project

---
+++ components/portfolio/project-detail.tsx
@@ -0,0 +1,12 @@
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+
+export const ProjectDetail = ({ details }: { details: string }) => {
+  return (
+    <div className="relative w-96 lg:w-[600px] max-w-md px-3 py-6">
+      <div className="prose max-h-[70%] overflow-scroll">
+        <ReactMarkdown className="dark:text-white" rehypePlugins={[rehypeRaw]}>{details}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -97,7 +97,9 @@ module.exports = {
     },
     
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 };
 
 
